75f1d4ee52533e518b124bf6921137733f3c1918
Modhak <modhak650@gmail.com>
2025-11-12 12:01:22 +0530

    Homepage

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext"; // adjust path
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
